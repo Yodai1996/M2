13f04c61926e500434fd03f40991f9d3a9c445ab
Takahiro Suzuki <taiyoyodai@gmail.com>
2021-12-25 12:23:33 +0900

    modify for saving the outputs at the quota at hospital or gk36

--- WisteriaShellScript/minmin_gk36.sh
+++ WisteriaShellScript/minmin_gk36.sh
@@ -6,7 +6,7 @@
 #PJM -L elapse=47:00:00
 #PJM --fs /work,/data
 #PJM -N minmin-training
-#PJM -o minmin.txt
+#PJM -o minmin_gk36.txt
 #PJM -j
 #PJM --mail-list suzuki-takahiro596@g.ecc.u-tokyo.ac.jp
 #PJM -m b
@@ -16,11 +16,9 @@ module load cuda/11.1
 
 cd "${PJM_O_WORKDIR}" || exit
 
-#保存先は東大病院なので注意。
+fold=1 #modify here. 5-fold CV.
 
-fold=2 #modify here. 5-fold CV.
-
-pretrained="ImageNet" #'BigBbox' #"ImageNet"
+pretrained='BigBbox' #"ImageNet"
 metric="FAUC_Pretrained${pretrained}"
 
 optimizer="VSGD" #epsilon=0.01
@@ -48,21 +46,20 @@ testBboxName="rare_small_bboxInfo_81_${fold}_withNormal"
 #testBbox="${testBboxName}.csv"
 
 #make dir for model and log
-modelPath="/work/jh170036a/k77012/M2/model/minmin_${metric}_${optimizer}_epoch${epoch}/"
+modelPath="/work/gk36/k77012/M2/model/minmin_${metric}_${optimizer}_epoch${epoch}/"
 mkdir -p ${modelPath}
-trainLog="/work/jh170036a/k77012/M2/train_log/minmin_${metric}_${optimizer}_epoch${epoch}/"
-mkdir -p ${trainLog}
+mkdir -p "/work/gk36/k77012/M2/train_log/minmin_${metric}_${optimizer}_epoch${epoch}/"
 
-for i in 20 21 22 23 24 25 26 27 28  #30 31 32 33 34 35 36 37 38 39 40
+for i in 17 18 19 20 21 22 23 24 25 #30 31 32 33 34 35 36 37 38 39 40
 do
   cd ../bo_io
   ./build/suggest --hm --ha --hpopt -a ei --md 5 --mi ./in/${boText} >> ../${bufText}
 
   trainPath="sim${i}_${m}"
-  abnormalDir="/work/jh170036a/k77012/M2/data/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/${trainPath}/"
-  segMaskDir="/work/jh170036a/k77012/M2/SegmentationMask/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/mask${i}_${m}/"
-  paraDir="/work/jh170036a/k77012/M2/simDataInfo/paraInfo/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/"
-  bboxDir="/work/jh170036a/k77012/M2/simDataInfo/bboxInfo/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/"
+  abnormalDir="/work/gk36/k77012/M2/data/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/${trainPath}/"
+  segMaskDir="/work/gk36/k77012/M2/SegmentationMask/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/mask${i}_${m}/"
+  paraDir="/work/gk36/k77012/M2/simDataInfo/paraInfo/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/"
+  bboxDir="/work/gk36/k77012/M2/simDataInfo/bboxInfo/minmin_${metric}_${optimizer}_epoch${epoch}_${fold}/"
   paraPath="${paraDir}/parameterInfo${i}_${m}.csv"
   bboxPath="${bboxDir}/bboxInfo${i}_${m}.csv"
 
@@ -73,12 +70,12 @@ do
 
   savePath="minmin_${metric}_${optimizer}_epoch${epoch}/${trainPath}_${validBboxName}_${testBboxName}_${model}_batch${batch_size}_pretrained${pretrained}"
 
-  mkdir -p "/work/jh170036a/k77012/M2/result/${savePath}/" #for saving Dir
-  mkdir -p "/work/jh170036a/k77012/M2/result/${savePath}/train"
-  mkdir -p "/work/jh170036a/k77012/M2/result/${savePath}/valid"
-  mkdir -p "/work/jh170036a/k77012/M2/result/${savePath}/test"
+  mkdir -p "/work/gk36/k77012/M2/result/${savePath}/" #for saving Dir
+  mkdir -p "/work/gk36/k77012/M2/result/${savePath}/train"
+  mkdir -p "/work/gk36/k77012/M2/result/${savePath}/valid"
+  mkdir -p "/work/gk36/k77012/M2/result/${savePath}/test"
 
-  pipenv run python ../WisteriaCodes/minmin.py $i ${boText} ${bufText} ${normalIdList} ${normalDir} ${abnormalDir} ${segMaskDir} ${paraPath} ${bboxPath} ${validPath} ${testPath} ${savePath} ${validBboxName} ${testBboxName} ${model} ${pretrained} ${epoch} ${batch_size} ${numSamples} ${modelPath} ${metric} ${optimizer} >> ${trainLog}/sim${i}_${trainPath}_${validBboxName}_${testBboxName}_${model}_batchsize${batch_size}_${pretrained}.txt
+  pipenv run python ../WisteriaCodes/minmin_gk36.py $i ${boText} ${bufText} ${normalIdList} ${normalDir} ${abnormalDir} ${segMaskDir} ${paraPath} ${bboxPath} ${validPath} ${testPath} ${savePath} ${validBboxName} ${testBboxName} ${model} ${pretrained} ${epoch} ${batch_size} ${numSamples} ${modelPath} ${metric} ${optimizer} >> ../train_log/minmin_${metric}_${optimizer}_epoch${epoch}/sim${i}_${trainPath}_${validBboxName}_${testBboxName}_${model}_batchsize${batch_size}_${pretrained}.txt
   echo $i'_finished'
 
 done
