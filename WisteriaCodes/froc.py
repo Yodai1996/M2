b8c1eb592be3dc4c5a4f2548c4b3bfe6618a84fe
Takahiro Suzuki <taiyoyodai@gmail.com>
2022-04-18 18:58:02 +0900

    modify to caluculate each respectively

--- WisteriaCodes/froc.py
+++ WisteriaCodes/froc.py
@@ -32,9 +32,9 @@ just for making a result
 
 args = sys.argv
 #Model Path
-saveFROCPath, end, nonrarePath = args[1], float(args[2]), args[3]
-BayRnPaths = args[4], args[5], args[6], args[7], args[8]
-CDRPaths = args[9], args[10], args[11], args[12], args[13]
+saveFROCPath, end = args[1], float(args[2])
+UDRPath, BayRnPath, CDRPath, easy2hard1Path, easy2hard2Path = args[3], args[4], args[5], args[6], args[7]
+i = int(args[8])
 
 # hypara
 originalSize = 1024
@@ -50,67 +50,77 @@ transform = transforms.Compose([
 #linestyles=[(0, (1, 0)), (0, (1, 1)), (0, (5, 1)), (0, (5, 1, 1, 1)),  ]
 plt.figure()
 
-for i in range(5):
-    bbox = "rare_small_bboxInfo_81_{}_withNormal.csv".format(str(i+1))
-    dataPath = 'AllDataDir'
-    dataDir = "/work/gk36/k77012/M2/data/{}/".format(dataPath)
-    df = pd.read_csv("/work/gk36/k77012/M2/{}".format(bbox))
 
-    df = preprocess_df(df, originalSize, size, dataDir)
-    dataset = MyDataset(df, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=4,  collate_fn=collate_fn)
+#for i in range(5):
+bbox = "rare_small_bboxInfo_81_{}_withNormal.csv".format(str(i))
+dataPath = 'AllDataDir'
+dataDir = "/work/gk36/k77012/M2/data/{}/".format(dataPath)
+df = pd.read_csv("/work/gk36/k77012/M2/{}".format(bbox))
 
-    #load model
-    BayRnPath = BayRnPaths[i]
-    CDRPath = CDRPaths[i]
+df = preprocess_df(df, originalSize, size, dataDir)
+dataset = MyDataset(df, transform=transform)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, pin_memory=True, num_workers=4,  collate_fn=collate_fn)
 
-    num_classes = 2 #(len(classes)) + 1
+num_classes = 2 #(len(classes)) + 1
 
-    #nonrare
-    nonraremodel = models.detection.ssd300_vgg16().to(device)
-    model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
-    nonraremodel.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
-    nonraremodel.load_state_dict(torch.load(nonrarePath))
+#UDR
+UDRmodel = models.detection.ssd300_vgg16().to(device)
+model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
+UDRmodel.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
+UDRmodel.load_state_dict(torch.load(UDRPath))
 
-    #BayRn
-    Baymodel = models.detection.ssd300_vgg16().to(device)
-    model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
-    Baymodel.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
-    Baymodel.load_state_dict(torch.load(BayRnPath))
+#BayRn
+Baymodel = models.detection.ssd300_vgg16().to(device)
+model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
+Baymodel.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
+Baymodel.load_state_dict(torch.load(BayRnPath))
 
-    #CDR
-    CDRmodel = models.detection.ssd300_vgg16().to(device)
-    model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
-    CDRmodel.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
-    CDRmodel.load_state_dict(torch.load(CDRPath))
+#CDR
+CDRmodel = models.detection.ssd300_vgg16().to(device)
+model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
+CDRmodel.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
+CDRmodel.load_state_dict(torch.load(CDRPath))
 
-    #inference
-    #nonrare
-    TPRs, FPIs, thresholds = FROC(dataloader, nonraremodel, ignore_big_bbox=True)
-    TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
-    if i==0:
-        plt.plot(FPIs, TPRs, "g", label="nonrare pneumonia dataset", linestyle=":")
-    else:
-        plt.plot(FPIs, TPRs, "g", linestyle=":")
+#easy2hard1
+easy2hard1model = models.detection.ssd300_vgg16().to(device)
+model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
+easy2hard1model.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
+easy2hard1model.load_state_dict(torch.load(easy2hard1Path))
 
-    # BayRn
-    TPRs, FPIs, thresholds = FROC(dataloader, Baymodel, ignore_big_bbox=True)
-    TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
-    if i == 0:
-        plt.plot(FPIs, TPRs, "b", label="BayRn", linestyle="--")
-    else:
-        plt.plot(FPIs, TPRs, "b", linestyle="--")
+#easy2hard2
+easy2hard2model = models.detection.ssd300_vgg16().to(device)
+model_head = models.detection.ssd300_vgg16(num_classes=num_classes)
+easy2hard2model.head.classification_head = model_head.head.classification_head.to(device)  # modify the head of the model
+easy2hard2model.load_state_dict(torch.load(easy2hard2Path))
 
-    # CDR
-    TPRs, FPIs, thresholds = FROC(dataloader, CDRmodel, ignore_big_bbox=True)
-    TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
-    if i == 0:
-        plt.plot(FPIs, TPRs, "r", label="CDR", linestyle="-")
-    else:
-        plt.plot(FPIs, TPRs, "r", linestyle="-")
+# UDR
+TPRs, FPIs, thresholds = FROC(dataloader, UDRmodel, ignore_big_bbox=True)
+TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
+plt.plot(FPIs, TPRs, label="UDR (Adam)", linestyle=(0, (1, 0)))
 
+# BayRn
+TPRs, FPIs, thresholds = FROC(dataloader, Baymodel, ignore_big_bbox=True)
+TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
+plt.plot(FPIs, TPRs, label="BayRn (Adam)", linestyle=(0, (1, 1)))
+
+# CDR
+TPRs, FPIs, thresholds = FROC(dataloader, CDRmodel, ignore_big_bbox=True)
+TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
+plt.plot(FPIs, TPRs, label="GDR (NVRM-SGD)", linestyle=(0, (3, 1)))
+
+#e2h1
+TPRs, FPIs, thresholds = FROC(dataloader, easy2hard1model, ignore_big_bbox=True)
+TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
+plt.plot(FPIs, TPRs, label="Easy2Hard-1 (NVRM-SGD)", linestyle=(0, (5, 1, 1, 1)))
+
+#e2h2
+TPRs, FPIs, thresholds = FROC(dataloader, easy2hard2model, ignore_big_bbox=True)
+TPRs, FPIs = modifyArea(TPRs, FPIs, include_FPIs=end)
+plt.plot(FPIs, TPRs, label="Easy2Hard-2 (NVRM-SGD)", linestyle=(0, (8, 1)))
 
 plt.xlabel('FPs/Image')
 plt.ylabel('TPR')
 plt.legend()
-plt.savefig(saveFROCPath + f"FROC_end{end}.png")
+plt.savefig(saveFROCPath + f"FROC_fold{i}_end{end}.png")
+
+
--- WisteriaShellScript/froc.sh
+++ WisteriaShellScript/froc.sh
@@ -3,7 +3,7 @@
 #PJM -g jh170036a
 #PJM -L rscgrp=share
 #PJM -L gpu=1
-#PJM -L elapse=30:00:00
+#PJM -L elapse=03:00:00
 #PJM --fs /work,/data
 #PJM -N froc
 #PJM -o froc.txt
@@ -22,38 +22,49 @@ cd "${PJM_O_WORKDIR}" || exit
 #pretrain="ImageNet"
 
 #endpoint to draw FROC
-end=2
+end=1
 
 prefixgk36="/work/gk36/k77012/M2/model"
-prefix="/work/jh170036a/k77012/M2/model"
-
+#prefix="/work/jh170036a/k77012/M2/model"
+prefix="/work/gu14/k77012/M2/model"
 
 #ModelPath
-nonrare="${prefixgk36}/pretrain/model_nonSmall_bboxInfo_655_nonSmall_bboxInfo_164_withNormal_VSGD_0.01_120"
+#nonrare="${prefixgk36}/pretrain/model_nonSmall_bboxInfo_655_nonSmall_bboxInfo_164_withNormal_VSGD_0.01_120"
 
 #minmin Path
 #change here
-Path1="minmin_FAUC_PretrainedImageNet_VSGD_epoch120/model_version30_rare_small_bboxInfo_20_1_withNormal_pretrainedImageNet_epoch120"
-Path2="minmin_FAUC_PretrainedImageNet_VSGD_epoch120/model_version19_rare_small_bboxInfo_20_2_withNormal_pretrainedImageNet_epoch120"
-Path3="minmin_FAUC_PretrainedImageNet_VSGD_epoch120/model_version21_rare_small_bboxInfo_20_3_withNormal_pretrainedImageNet_epoch120"
-Path4="minmin_FAUC_PretrainedImageNet_VSGD_epoch120/model_version23_rare_small_bboxInfo_20_4_withNormal_pretrainedImageNet_epoch120"
-Path5="minmin_FAUC_PretrainedImageNet_VSGD_epoch120/model_version32_rare_small_bboxInfo_20_5_withNormal_pretrainedImageNet_epoch120"
+#Path1="minmin_FAUC_PretrainedImageNet_Adam_epoch120/model_version40_rare_small_bboxInfo_20_1_withNormal_pretrainedImageNet_epoch120"
+#Path2="minmin_FAUC_PretrainedImageNet_Adam_epoch120/model_version14_rare_small_bboxInfo_20_2_withNormal_pretrainedImageNet_epoch120"
+#Path3="minmin_FAUC_PretrainedImageNet_Adam_epoch120/model_version40_rare_small_bboxInfo_20_3_withNormal_pretrainedImageNet_epoch120"
+#Path4="minmin_FAUC_PretrainedImageNet_Adam_epoch120/model_version17_rare_small_bboxInfo_20_4_withNormal_pretrainedImageNet_epoch120"
+#Path5="model_version3_sim3_1000_rare_small_bboxInfo_20_5_withNormal_ImageNet_Adam_epoch120"
+Path1="minmin_FAUC_PretrainedImageNet_Adam_epoch120_ver1/model_version40_rare_small_bboxInfo_20_1_withNormal_pretrainedImageNet_epoch120"
+Path2="minmin_FAUC_PretrainedImageNet_Adam_epoch120_ver1/model_version14_rare_small_bboxInfo_20_2_withNormal_pretrainedImageNet_epoch120"
+Path3="minmin_FAUC_PretrainedImageNet_Adam_epoch120_ver1/model_version40_rare_small_bboxInfo_20_3_withNormal_pretrainedImageNet_epoch120"
+Path4="minmin_FAUC_PretrainedImageNet_Adam_epoch120/model_version17_rare_small_bboxInfo_20_4_withNormal_pretrainedImageNet_epoch120"
+Path5="model_version3_sim3_1000_rare_small_bboxInfo_20_5_withNormal_ImageNet_Adam_epoch120"
 
-BayRn1="${prefixgk36}/${Path1}"
+#BayRn1="${prefixgk36}/${Path1}"
+BayRn1="${prefix}/${Path1}"
 BayRn2="${prefix}/${Path2}"
 BayRn3="${prefix}/${Path3}"
 BayRn4="${prefix}/${Path4}"
-BayRn5="${prefix}/${Path5}"
+BayRn5="${prefixgk36}/${Path5}"
 
-#CDR Path
-#change here
-#Path1="curriculumBO/rare_small_bboxInfo_20_1_withNormal/start0.5_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch40/model7"
-#Path2="curriculumBO/rare_small_bboxInfo_20_2_withNormal/start0.6_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch40/model9"
-#Path3="curriculumBO/rare_small_bboxInfo_20_3_withNormal/start0.6_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch40/model10"
-#Path4="curriculumBO/rare_small_bboxInfo_20_4_withNormal/start0.5_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch40/model6"
-#Path5="curriculumBO/rare_small_bboxInfo_20_5_withNormal/start0.5_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch40/model5"
-#
-#CDR1="${prefixgk36}/${Path1}"
+#UDR
+Path1="udr/model_udr_1000_rare_small_bboxInfo_20_1_withNormal_ImageNet_Adam_epoch120"
+Path2="udr/model_udr_1000_rare_small_bboxInfo_20_2_withNormal_ImageNet_Adam_epoch120"
+Path3="udr/model_udr_1000_rare_small_bboxInfo_20_3_withNormal_ImageNet_Adam_epoch120"
+Path4="udr/model_udr_1000_rare_small_bboxInfo_20_4_withNormal_ImageNet_Adam_epoch120"
+Path5="udr/model_udr_1000_rare_small_bboxInfo_20_5_withNormal_ImageNet_Adam_epoch120"
+
+UDR1="${prefix}/${Path1}"
+UDR2="${prefix}/${Path2}"
+UDR3="${prefix}/${Path3}"
+UDR4="${prefix}/${Path4}"
+UDR5="${prefix}/${Path5}"
+
+#GDR Path
 Path1="curriculumBO/rare_small_bboxInfo_20_1_withNormal/start0.6_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model5"
 Path2="curriculumBO/rare_small_bboxInfo_20_2_withNormal/start0.6_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model2"
 Path3="curriculumBO/rare_small_bboxInfo_20_3_withNormal/start0.6_decay1.0_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model5"
@@ -66,8 +77,45 @@ CDR3="${prefix}/${Path3}"
 CDR4="${prefix}/${Path4}"
 CDR5="${prefix}/${Path5}"
 
-saveFROCPath="/work/jh170036a/k77012/M2/FROC/All_ImageNetPretrain/" #saveFROCPath="/work/gk36/k77012/M2/FROC/${Path}/${dataBboxName}/"
+#easy2hard-1
+Path1="curriculumBO/rare_small_bboxInfo_20_1_withNormal/start1.0_decay0.1_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model8"
+Path2="curriculumBO/rare_small_bboxInfo_20_2_withNormal/start1.0_decay0.1_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model4"
+Path3="curriculumBO/rare_small_bboxInfo_20_3_withNormal/start1.0_decay0.1_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model5"
+Path4="curriculumBO/rare_small_bboxInfo_20_4_withNormal/start1.0_decay0.1_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model5"
+Path5="curriculumBO/rare_small_bboxInfo_20_5_withNormal/start1.0_decay0.1_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model6"
+
+easy2hard1_1="${prefix}/${Path1}"
+easy2hard1_2="${prefix}/${Path2}"
+easy2hard1_3="${prefix}/${Path3}"
+easy2hard1_4="${prefix}/${Path4}"
+easy2hard1_5="${prefix}/${Path5}"
+
+#easy2hard-2
+Path1="curriculumBO/rare_small_bboxInfo_20_1_withNormal/start0.75_decay0.05_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model9"
+Path2="curriculumBO/rare_small_bboxInfo_20_2_withNormal/start0.75_decay0.05_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model5"
+Path3="curriculumBO/rare_small_bboxInfo_20_3_withNormal/start0.75_decay0.05_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model5"
+Path4="curriculumBO/rare_small_bboxInfo_20_4_withNormal/start0.75_decay0.05_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model4"
+Path5="curriculumBO/rare_small_bboxInfo_20_5_withNormal/start0.75_decay0.05_VSGD_FAUC_PretrainedImageNet_variability0.01_decay1.0_t1000_v200_iter30_inf30_epoch120/model7"
+
+easy2hard2_1="${prefix}/${Path1}"
+easy2hard2_2="${prefix}/${Path2}"
+easy2hard2_3="${prefix}/${Path3}"
+easy2hard2_4="${prefix}/${Path4}"
+easy2hard2_5="${prefix}/${Path5}"
+
+
+saveFROCPath="/work/gu14/k77012/M2/FROC/" #"/work/gu14/k77012/M2/FROC/All_ImageNetPretrain/" #saveFROCPath="/work/gk36/k77012/M2/FROC/${Path}/${dataBboxName}/"
 mkdir -p ${saveFROCPath}
 
-pipenv run python ../WisteriaCodes/froc.py ${saveFROCPath} ${end} ${nonrare} ${BayRn1} ${BayRn2} ${BayRn3} ${BayRn4} ${BayRn5} ${CDR1} ${CDR2} ${CDR3} ${CDR4} ${CDR5}
+####change below####
+fold=5
+UDR=${UDR5}
+BayRn=${BayRn5}
+CDR=${CDR5}
+easy2hard1=${easy2hard1_5}
+easy2hard2=${easy2hard2_5}
+####change above####
+
+#pipenv run python ../WisteriaCodes/froc.py ${saveFROCPath} ${end} ${UDR1} ${UDR2} ${UDR3} ${UDR4} ${UDR5} ${BayRn1} ${BayRn2} ${BayRn3} ${BayRn4} ${BayRn5} ${CDR1} ${CDR2} ${CDR3} ${CDR4} ${CDR5} ${easy2hard1_1} ${easy2hard1_2} ${easy2hard1_3} ${easy2hard1_4} ${easy2hard1_5} ${easy2hard2_1} ${easy2hard2_2} ${easy2hard2_3} ${easy2hard2_4} ${easy2hard2_5}
+pipenv run python ../WisteriaCodes/froc.py ${saveFROCPath} ${end} ${UDR} ${BayRn} ${CDR} ${easy2hard1} ${easy2hard2} ${fold}
 echo 'froc_finished'
