f6ad3223a8e19d63388bcd7e169970d39d0bbef8
Takahiro Suzuki <taiyoyodai@gmail.com>
2022-04-30 18:14:09 +0900

    refactoring fractalGenerator.py

--- WisteriaCodes/fractalGenerator.py
+++ WisteriaCodes/fractalGenerator.py
@@ -30,14 +30,11 @@ def scalingAndBluring(scaledMask, r, center, smoothArea):
     return scaledMask
 
 def lungPosition(img, pneumonia, width, height, margin, too_white, iter_cnt, increment):
-    # decide the position where the pseudo pneumonia is put on, avoiding the too white position
+    # decide the position where the pseudo pneumonia lesion is put on, avoiding too white positions
     size = img.size[0]  # 1024
 
     cnt = 0
     while (1):
-        # center = rand(2) * (size - margin * 2) + margin
-        # centX = int(center[0])
-        # centY = int(center[1])
         centX = int(random.random() * (size - margin * 2) + margin)
         centY = int(random.random() * (size - margin * 2) + margin)
 
@@ -50,7 +47,7 @@ def lungPosition(img, pneumonia, width, height, margin, too_white, iter_cnt, inc
                 if pneumonia[j][i]>0:
                     num += 1
                     x, y = int(centX - width // 2 + i), int(centY - height // 2 + j)
-                    s += img.getpixel((x, y)) #imgPixels[y][x] でもよい
+                    s += img.getpixel((x, y))
 
         if not s > too_white * num:
             break
@@ -69,23 +66,19 @@ def make_abnormal(newImg, segMaskImg, index, lb, ub, res, octaves, persistence, 
     r = (ub - lb) * random.random() + lb
     r = int(r)
 
-    #### make the white circle on the canvas
+    #make a white circle on the canvas
     mask_im = Image.new("L", (canvasSize, canvasSize), 0)
     draw = ImageDraw.Draw(mask_im)
     draw.ellipse((center - r, center - r, center + r, center + r), fill=255)
 
-    #### make the circle blured and scaled
+    #make the circle blured and scaled
     mask = np.array(mask_im)
     scaledMask = mask * scale
-    scaledMask = scalingAndBluring(scaledMask, r + 1, center, smoothArea)  #to delete noise we use r+1 instead of r
-    pilScaledMask = Image.fromarray(scaledMask) # translate np.araray into PIL object
-    pilScaledMask = pilScaledMask.convert("L")
+    scaledMask = scalingAndBluring(scaledMask, r + 1, center, smoothArea)  #we use r+1 instead of r to delete noise around the edges
 
-    #### apply affine transformation randomly
+    #apply an affine transformation randomly
     roi_points = [(0, 0), (canvasSize, 0), (canvasSize, canvasSize), (0, canvasSize)]
-    # x1, y1, x2, y2 = (2 * r * rand(4) + center - r) #円のbbox内からランダムに選ばれるというモデルでまず試してみる。
-    # x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-    x1, y1, x2, y2 = [int(2 * r * random.random() + center - r) for _ in range(4)] #円のbbox内からランダムに選ばれるというモデルでまず試してみる。
+    x1, y1, x2, y2 = [int(2 * r * random.random() + center - r) for _ in range(4)] #points are chosen randomly inside the circle
     from_points = [(x1, y1)]
     to_points = [(x2, y2)]
     from_points = np.concatenate((roi_points, from_points))
@@ -93,8 +86,6 @@ def make_abnormal(newImg, segMaskImg, index, lb, ub, res, octaves, persistence, 
     affin = skimage.transform.PiecewiseAffineTransform()
     success = affin.estimate(to_points, from_points)
     assert(success == True)
-    # normalizedTransformedMask = skimage.transform.warp(pilScaledMask, affin)  # [0,scale)
-    # transformedMask = np.array(normalizedTransformedMask * 255., dtype=np.uint8)  # [0, 255*scale]
     transformedMask = skimage.transform.warp(scaledMask, affin)  # [0,scale)
     normalizedTransformedMask = transformedMask / 256
 
@@ -106,13 +97,12 @@ def make_abnormal(newImg, segMaskImg, index, lb, ub, res, octaves, persistence, 
     width = xmax - x
     height = ymax - y
 
-    #### generate fractal perlin noise
+    #generate fractal perlin noise
     shape = (lacunarity**(octaves - 1)) * res
     np.random.seed(index)
     noise = generate_fractal_noise_2d(shape=(shape, shape), res=(res, res), octaves=octaves, persistence=persistence, lacunarity=lacunarity)
 
-    #normalizing
-    noise = (noise+1)/2  #0~1に正規化
+    noise = (noise+1)/2  #normalize between 0~1
 
     #resize the image
     resized_noise = resize(noise, (height + 1, width + 1))
@@ -125,12 +115,12 @@ def make_abnormal(newImg, segMaskImg, index, lb, ub, res, octaves, persistence, 
     for j in range(height):
         for i in range(width):
             x, y = int(centX - width // 2 + i), int(centY - height // 2 + j)
-            inPixel = newImg.getpixel((x, y)) #imgPixels[y][x]でもよい
+            inPixel = newImg.getpixel((x, y))
             pneumoniaPixel = pneumonia[j][i]
             if pneumoniaPixel > 0:
-                outPixel = inPixel * (1 - pneumoniaPixel) + 255 * pneumoniaPixel #X線レントゲン原理
+                outPixel = inPixel * (1 - pneumoniaPixel) + 255 * pneumoniaPixel #apply X-ray principle
                 newImg.putpixel((x, y), int(outPixel))
-                segMaskImg.putpixel((x, y), 255) #make segmentation mask
+                segMaskImg.putpixel((x, y), 255) #make a segmentation mask
 
     left, top, right, bottom = centX - width / 2, centY - height / 2, centX + width / 2, centY + height / 2
 
@@ -139,17 +129,16 @@ def make_abnormal(newImg, segMaskImg, index, lb, ub, res, octaves, persistence, 
 
 def make_abnormals(ll, normal_dir, abnormal_dir, segMask_dir, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea):
 
-    #病変を何個作成するか。
+    #How many lesions are inserted into a normal image is determined by the following probability (based on the observation of #small-lesions in 101 dataset)
     prob1 = 0.93
     prob2 = 0.04
     prob3 = 0.03
 
-    # こっちは縦につなげるか。見やすいので。
     parameterInfo = []
     parameterInfo.append(["file", "index", "lb", "ub", "res", "octaves", "persistence", "lacunarity", "scale", "smoothArea", "cent_x", "cent_y", "r"])
 
     bboxInfo = []
-    bboxInfo.append(["file", "left1", "top1", "right1", "bottom1", "left2", "top2", "right2", "bottom2", "left3", "top3", "right3", "bottom3", "left4", "top4", "right4", "bottom4"]) #x,y,xmax,ymax #こっちは横につなげよう。無ければNoneでよい.
+    bboxInfo.append(["file", "left1", "top1", "right1", "bottom1", "left2", "top2", "right2", "bottom2", "left3", "top3", "right3", "bottom3", "left4", "top4", "right4", "bottom4"])
 
     index = 0
 
@@ -172,27 +161,27 @@ def make_abnormals(ll, normal_dir, abnormal_dir, segMask_dir, lb, ub, res, octav
         left4, top4, right4, bottom4 = None, None, None, None
 
         #make abnormals
-        #１個目作成
+        #make the first lesion
         newImg, segMaskImg, left1, top1, right1, bottom1, centX, centY, r = make_abnormal(newImg, segMaskImg, index, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea)
-        parameterInfo.append([file, index, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea, centX, centY, r])  # ほかのパラメタも一応入れとく。
+        parameterInfo.append([file, index, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea, centX, centY, r])
         index += 1
         if prob1 < prob:
-            #2個目も作成
+            #second
             newImg, segMaskImg, left2, top2, right2, bottom2, centX, centY, r = make_abnormal(newImg, segMaskImg, index,
                                                                                               lb, ub, res, octaves,
                                                                                               persistence, lacunarity,
                                                                                               scale, smoothArea)
-            parameterInfo.append([file, index, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea, centX, centY, r])  # ほかのパラメタも一応入れとく。
+            parameterInfo.append([file, index, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea, centX, centY, r])
             index += 1
             if prob1+prob2 < prob:
-                # 3個目も作成
+                #third
                 newImg, segMaskImg, left3, top3, right3, bottom3, centX, centY, r = make_abnormal(newImg, segMaskImg,
                                                                                                   index,
                                                                                                   lb, ub, res, octaves,
                                                                                                   persistence,
                                                                                                   lacunarity,
                                                                                                   scale, smoothArea)
-                parameterInfo.append([file, index, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea, centX, centY, r])  # ほかのパラメタも一応入れとく。
+                parameterInfo.append([file, index, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea, centX, centY, r])
                 index += 1
 
         bboxInfo.append([file, left1, top1, right1, bottom1, left2, top2, right2, bottom2, left3, top3, right3, bottom3, left4, top4, right4, bottom4])
@@ -217,29 +206,26 @@ if __name__ == '__main__':
         ll = list(read)
     f.close()
 
-    # 一応両方バージョン書いとく。
-    if len(ll) <= 2: #１行版
-        ll = ll[0] # [ll[0][i] for i in range(len(ll))]
-    else: #改行版
+    #deal with both indexing formats
+    if len(ll) <= 2:
+        ll = ll[0]
+    else:
         ll = [ll[i][0] for i in range(len(ll))]
 
 
     if len(args)>9:
-        #values are given by hands
-
-        #fix these for small bbox detection
-        lb=20 #int(args[7]) #20
-        ub=75 #int(args[8]) #75
+        #values are directly given from command line
+        lb=20
+        ub=75
         res=int(args[7])
-        octaves=5 #int(args[10])
+        octaves=5
         persistence=float(args[8])
         lacunarity=int(args[9])
         scale=float(args[10])
         smoothArea=float(args[11])
 
     elif len(args)<=8:
-        #len(args)==8
-        #values are given through bufText
+        #values are normalized and given through a text file
         bufText=args[7]
 
         # read the recommended next values from Gaussian Process.
@@ -254,11 +240,10 @@ if __name__ == '__main__':
         values = last_lines.split(",")
         values = [float(i) for i in values]
 
-        # preprocess
-        # fix these for small bbox detection
-        lb = 20  # int(30 + 50 * values[0])  #[30,80]
-        ub = 75  # int(100 + 120 * values[1]) #[100,220]
-        octaves = 5  # fixed
+        # decode the parameters from the normalized values
+        lb = 20
+        ub = 75
+        octaves = 5
         res = int(2 + 4 * values[0])
         persistence = 0.2 + 0.8 * values[1]  # [0.2,1]
         lacunarity = int(2 + 3 * values[2])
@@ -269,18 +254,17 @@ if __name__ == '__main__':
         print("arguments error happening")
         exit()
 
-    # make abnormal data
+    #make abnormal data
     print("begin to make abnormal images")
-    #parameterInfo, bboxInfo = make_abnormal(ll, normal_dir, abnormal_dir, segMask_dir, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea)
     parameterInfo, bboxInfo = make_abnormals(ll, normalDir, abnormalDir, segMaskDir, lb, ub, res, octaves, persistence, lacunarity, scale, smoothArea)
     print("finish making abnormal images")
 
     #store the information about parameters to reproduce the same images
     with open(saveParaPath, 'w') as f:
         writer = csv.writer(f, lineterminator="\n")
-        writer.writerows(parameterInfo) #instead of writerow
+        writer.writerows(parameterInfo)
 
     #store the information about bbox to use it in model learning
     with open(saveBboxPath, 'w') as f:
         writer = csv.writer(f, lineterminator="\n")
-        writer.writerows(bboxInfo) #instead of writerow
+        writer.writerows(bboxInfo)
